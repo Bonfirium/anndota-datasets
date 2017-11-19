f827a223a937c64ae08dc030baa26bd468f5fc86
NickLatkovich <nicklatkovich@gmail.com>
2017-11-19 07:50:25 +0300

    Add ann-index of every hero

--- heroesParser.js
+++ heroesParser.js
@@ -5,9 +5,10 @@ const Utils = require('./utils')
 async function main( ) {
     let heroes = await Utils.getFetch(Utils.openDotaAPI + 'heroes')
     let result = { }
-    heroes.forEach(function(hero) {
+    heroes.forEach(function(hero, index) {
         result[hero.id] = {
-            name: hero.localized_name
+            name: hero.localized_name,
+            ann_index: index
         }
     }, this)
     fs.writeFileSync('data/heroes.json', JSON.stringify(result))
